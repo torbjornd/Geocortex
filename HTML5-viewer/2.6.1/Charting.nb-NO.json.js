de09950e63b6b6ccb6bdcf8e8c47cf432c03e460
Torbjørn Dalløkken <torbjorn@dallokken.com>
2017-01-20 14:51:14 +0100

    Norwegian translation

---
+++ HTML5-viewer/2.6.1/Charting.nb-NO.json.js
@@ -0,0 +1,6 @@
+﻿{
+     "language-charting-chart": "Diagram",
+     "language-charting-chart-name": "Diagramnavn",
+     "language-charting-chart-series": "Visualisert diagramserie",
+     "language-charting-chart-type": "Diagramtype"
+}
---
+++ HTML5-viewer/2.6.1/Mapping.Charting.nb-NO.json.js
@@ -0,0 +1,11 @@
+﻿{
+     "language-charting-no-items": "Ingen diagrammer å vise",
+     "language-charting-selector-text": "Velg diagram",
+     "language-feature-charts": "Diagram"
+     "language-charting-smart-panel-maximize-restore": "Maksimer/Oppfrisk fane",
+     "language-charting-smart-panel-resize": "Dra for å endre fanestørrelse",
+     "language-common-close": "Lukk",
+     "language-feature-no-chart": "Ingen diagrammer",
+     "language-framework-ui-view-container-view-back": "Gå tilbake {0}",
+     "language-framework-ui-view-container-view-close": "Lukk {0}"
+}
